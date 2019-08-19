1677a5aa433243f24587e531a58aa98d1b913fbd
ttagu99 <ttagu99@gmail.com>
2019-08-19 23:12:20 +0900

    add nsml ignore

---
+++ setup.py
@@ -0,0 +1,20 @@
+#nsml: nsml/ml:cuda9.0-cudnn7-tf-1.11torch1.0keras2.2
+
+from distutils.core import setup
+setup(
+    name='airush1',
+    version='1.0',
+    install_requires=[
+            '#tqdm',
+            #'torch>=1.1',
+            #'pickle-mixin',
+            #'torchvision',
+            #'pandas>=0.24.0',
+            #'scikit-multilearn',
+            #'cnn_finetune',
+            #'efficientnet-pytorch',
+            #'torchsummary',
+            #'Pillow'
+            #'iterative-stratification'
+    ]
+)
---
+++ submit.py
@@ -0,0 +1,46 @@
+import os
+import sys
+import time
+import subprocess
+from multiprocessing import Process
+from datetime import datetime
+import pandas as pd
+
+team_name = "team_27"       
+data_name = "airush1"     
+start_wait_sec =  21*60#35*60
+submit_list_path  = './submit_list_cand.csv'
+#airush1
+def run_submit(command):
+    now = time.time()
+    print(f"[Command] {command}")
+    print(datetime.now())
+    subprocess.call(command)
+    print(f"[Collapsed time] {time.time() - now}")
+
+if __name__ == "__main__":
+    S_HOUR = 3601
+    li_procs = []
+
+    while(1):
+        s_df = pd.read_csv(submit_list_path)
+        for i in range(s_df.shape[0]):
+            s = s_df.session[i]
+            m = s_df.model[i]
+            full_session = '/'.join([team_name, data_name, str(s)])
+            full_command = f"nsml submit {full_session} {m}"
+            li_procs.append(Process(target=run_submit, args=(full_command, )))
+
+        now = time.time()
+        time.sleep(start_wait_sec)
+
+        for i, proc in enumerate(li_procs):
+            proc.start()
+            if i + 1 < len(li_procs):
+                time.sleep(S_HOUR)
+
+        for proc in li_procs:
+            proc.join()
+        print(f"Total collapsed time: {time.time() - now}")
+        s_df.drop(s_df.index, inplace=True)
+        s_df.to_csv(submit_list_path)
